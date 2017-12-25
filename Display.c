81dcfbdb812f0d8486f2f4bf3f0019aad7eef41f
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2017-12-25 10:06:29 +0000

    Optimize display update

--- Display.c
+++ Display.c
@@ -5,33 +5,12 @@
 #include "LayerState.h"
 #include "ScanKeys.h"
 #include "Timer.h"
-#include <avr/io.h>
 #include <LUFA/Drivers/Peripheral/SPI.h>
 #include <LUFA/Drivers/USB/USB.h>
 #include <stdlib.h>
 #include <u8g.h>
 
-u8g_t u8g;
-uint8_t Display_LEDReport;
-uint8_t Display_keypad_state;
-uint8_t Display_Fn_state;
-uint8_t mode;
-uint8_t last_USB_DeviceState;
-uint32_t last_tick;
-
-// Screensaver
-uint8_t px;
-uint8_t py;
-uint8_t step;
-uint8_t start;
-uint32_t last_display_init;
-
-// Modes
-#define SPLASH_MODE 0
-#define STATUS_MODE 1
-#define SCREENSAVER_MODE 2
-
-// Configuration
+// Configuraiton
 #define SPLASH_TIMEOUT 2
 #define SCREENSAVER_TIMEOUT 180
 #define CONTRAST 96
@@ -40,18 +19,22 @@ uint32_t last_display_init;
 #define TOGGLE_HEIGHT  22
 #define TOGGLE_SPACING 2
 
+// Modes
+#define SPLASH_MODE 0
+#define STATUS_MODE 1
+#define SCREENSAVER_MODE 2
+
+static u8g_t u8g;
+static uint8_t Display_LEDReport;
+static uint8_t Display_keypad_state;
+static uint8_t Display_Fn_state;
+static uint8_t last_USB_DeviceState;
+static uint8_t mode;
+static uint32_t last_tick;
+
 /*
  * Initialization
  */
-
-void Display_Screensaver_Init(void)
-{
-  px = (float)rand() / RAND_MAX * u8g_GetWidth(&u8g);
-  py = (float)rand() / RAND_MAX * u8g_GetHeight(&u8g);
-  step = (float)rand() / RAND_MAX * 22 + 12;
-  start = (float)rand() / RAND_MAX * step;
-  last_display_init = Timer_Secs();
-}
 
 void Display_Init(void)
 {
@@ -72,8 +55,6 @@ void Display_Init(void)
   last_USB_DeviceState = USB_DeviceState;
 
   mode = SPLASH_MODE;
-
-  Display_Screensaver_Init();
 }
 
 /*
@@ -152,88 +133,60 @@ void Display_Draw_Toggle(
 }
 
 /*
- * Display Modes
+ * Modes
  */
 
-void Display_Splash(void)
+void Display_Splash(void *context)
 {
-  u8g_FirstPage(&u8g);
-  do {
-    u8g_DrawBitmapP(
-      &u8g,
-      ((u8g_GetWidth(&u8g) - (BITMAP_LOGO_WIDTH * 8)) / 2),
-      (u8g_GetHeight(&u8g) - BITMAP_LOGO_HEIGHT) / 2,
-      BITMAP_LOGO_WIDTH, BITMAP_LOGO_HEIGHT,
-      bitmap_logo
-    );
-  } while(u8g_NextPage(&u8g));
+  u8g_DrawBitmapP(
+    &u8g,
+    ((u8g_GetWidth(&u8g) - (BITMAP_LOGO_WIDTH * 8)) / 2),
+    (u8g_GetHeight(&u8g) - BITMAP_LOGO_HEIGHT) / 2,
+    BITMAP_LOGO_WIDTH, BITMAP_LOGO_HEIGHT,
+    bitmap_logo
+  );
 }
 
-/**< Internally implemented by the library. This state indicates
-  *   that the device is not currently connected to a host.
-  */
-void Display_USB_Unattached(void)
+struct StatusContext
 {
-  u8g_FirstPage(&u8g);
-  do {
-    u8g_SetFont(&u8g, u8g_font_helvB10);
-    Display_Write_CenteredP(U8G_PSTR("USB\ndisconnected"));
-  } while(u8g_NextPage(&u8g));
+    uint8_t USB_DeviceState;
+    bool USB_Device_RemoteWakeupEnabled;
+    uint8_t Right_Side_Disconnected;
+    uint8_t LEDReport;
+    u8g_pgm_uint8_t *KeyboardLayerName;
+    u8g_pgm_uint8_t *ComputerLayerName;
+    uint32_t Counter;
+};
+
+void *Display_Status_Context(void)
+{
+  static struct StatusContext context;
+
+  context.USB_DeviceState = USB_DeviceState;
+  context.USB_Device_RemoteWakeupEnabled = USB_Device_RemoteWakeupEnabled;
+  context.Right_Side_Disconnected = ScanKeys_Right_Side_Disconnected;
+  context.LEDReport = Display_LEDReport;
+  context.KeyboardLayerName = Keymap_Get_Layer_Keyboard_Name(LayerState_Get_Active_Layout());
+  context.ComputerLayerName = Keymap_Get_Layer_Computer_Name(LayerState_Get_Active_Layout());
+  context.Counter = Counter_Get();
+
+  return (void *)&context;
 }
 
-/**< Internally implemented by the library. This state indicates
-  *   that the device is connected to a host, but enumeration has not
-  *   yet begun.
-  */
-void Display_USB_Powered(void)
+void Display_Draw_LED_Status(uint8_t LEDReport)
 {
-  u8g_FirstPage(&u8g);
-  do {
-    u8g_SetFont(&u8g, u8g_font_helvB10);
-    Display_Write_CenteredP(U8G_PSTR("USB connected,\nwaiting for\nenumeration"));
-  } while(u8g_NextPage(&u8g));
-}
-
-/**< Internally implemented by the library. This state indicates
-  *   that the device's USB bus has been reset by the host and it is
-  *   now waiting for the host to begin the enumeration process.
-  */
-void Display_USB_Default(void)
-{
-  u8g_FirstPage(&u8g);
-  do {
-    u8g_SetFont(&u8g, u8g_font_helvB10);
-    Display_Write_CenteredP(U8G_PSTR("USB bus reset,\nwaiting for\nenumeration"));
-  } while(u8g_NextPage(&u8g));
-}
-
-/**< Internally implemented by the library. This state indicates
-  *   that the device has been addressed by the USB Host, but is not
-  *   yet configured.
-  */
-void Display_USB_Addressed(void)
-{
-  u8g_FirstPage(&u8g);
-  do {
-    u8g_SetFont(&u8g, u8g_font_helvB10);
-    Display_Write_CenteredP(U8G_PSTR("USB addressed,\nwaiting for\nconfiguration"));
-  } while(u8g_NextPage(&u8g));
-}
-
-void Display_Draw_LED_Status(void)
-{
-  if(Display_LEDReport != NO_LED_REPORT){
+  if(LEDReport != NO_LED_REPORT){
     Display_Draw_Toggle(
         0 * (TOGGLE_WIDTH - 2 + TOGGLE_SPACING), 0, TOGGLE_WIDTH - 2, TOGGLE_HEIGHT,
-        U8G_PSTR("1"), Display_LEDReport & HID_KEYBOARD_LED_NUMLOCK
+        U8G_PSTR("1"), LEDReport & HID_KEYBOARD_LED_NUMLOCK
     );
     Display_Draw_Toggle(
         1 * (TOGGLE_WIDTH - 2 + TOGGLE_SPACING), 0, TOGGLE_WIDTH - 2, TOGGLE_HEIGHT,
-        U8G_PSTR("A"), Display_LEDReport & HID_KEYBOARD_LED_CAPSLOCK
+        U8G_PSTR("A"), LEDReport & HID_KEYBOARD_LED_CAPSLOCK
     );
     Display_Draw_Toggle(
         2 * (TOGGLE_WIDTH - 2 + TOGGLE_SPACING), 0, TOGGLE_WIDTH - 2, TOGGLE_HEIGHT,
-        U8G_PSTR("S"), Display_LEDReport & HID_KEYBOARD_LED_SCROLLLOCK
+        U8G_PSTR("S"), LEDReport & HID_KEYBOARD_LED_SCROLLLOCK
     );
   }
   Display_Draw_Toggle(
@@ -246,7 +199,7 @@ void Display_Draw_LED_Status(void)
   );
 }
 
-void Display_Draw_Layout_Status(void)
+void Display_Draw_Layout_Status(u8g_pgm_uint8_t *KeyboardLayerName, u8g_pgm_uint8_t *ComputerLayerName)
 {
   uint8_t y;
   uint8_t x_icon;
@@ -260,162 +213,193 @@ void Display_Draw_Layout_Status(void)
   x_text = x_icon + BITMAP_KEYBOARD_WIDTH * 8;
   Display_Write_Box_CenteredP(
     x_text, y, u8g_GetWidth(&u8g) - x_text - x_icon, BITMAP_KEYBOARD_HEIGHT,
-    Keymap_Get_Layer_Keyboard_Name(LayerState_Get_Active_Layout())
+    KeyboardLayerName
   );
 
   y = y + BITMAP_COMPUTER_HEIGHT + 3;
   u8g_DrawBitmapP(&u8g, x_icon, y, BITMAP_COMPUTER_WIDTH, BITMAP_COMPUTER_HEIGHT, bitmap_computer);
   Display_Write_Box_CenteredP(
     x_text, y, u8g_GetWidth(&u8g) - x_text - x_icon, BITMAP_KEYBOARD_HEIGHT,
-    Keymap_Get_Layer_Computer_Name(LayerState_Get_Active_Layout())
+    ComputerLayerName
   );
 }
 
-void Display_Draw_Keypress_Counter(void)
+void Display_Status(void *in_context)
 {
-  char str[20];
+  struct StatusContext *context;
 
-  u8g_SetFont(&u8g, u8g_font_6x10);
-  ultoa(Counter_Get(), str, 10);
-  u8g_DrawStr(&u8g, u8g_GetWidth(&u8g) / 2 - u8g_GetStrWidth(&u8g, str) / 2, u8g_GetHeight(&u8g), str);
-}
+  context = (struct StatusContext *)in_context;
 
-/**< May be implemented by the user project. This state indicates
-  *   that the device has been enumerated by the host and is ready
-  *   for USB communications to begin.
-  */
-void Display_USB_Configured(void)
-{
-  if(ScanKeys_Right_Side_Disconnected) {
-    u8g_FirstPage(&u8g);
-    do {
-      u8g_SetFont(&u8g, u8g_font_helvB14);
-      Display_Write_CenteredP(U8G_PSTR("Right Side\nDisconnected"));
-    } while(u8g_NextPage(&u8g));
-  } else {
-    u8g_FirstPage(&u8g);
-    do {
-      u8g_SetFont(&u8g, u8g_font_helvB14);
-      Display_Draw_LED_Status();
-      Display_Draw_Layout_Status();
-      Display_Draw_Keypress_Counter();
-    } while(u8g_NextPage(&u8g));
-  }
-}
+  u8g_SetFont(&u8g, u8g_font_helvB10);
 
-/**< May be implemented by the user project. This state indicates
-  *   that the USB bus has been suspended by the host, and the device
-  *   should power down to a minimal power level until the bus is
-  *   resumed.
-  */
-void Display_USB_Suspended(void)
-{
-  u8g_pgm_uint8_t * strP;
-
-  if(USB_Device_RemoteWakeupEnabled)
-    strP = U8G_PSTR("USB suspended:\npress any key to\nwake up");
-  else
-    strP = U8G_PSTR("USB\nsuspended");
-  u8g_FirstPage(&u8g);
-  do {
-    u8g_SetFont(&u8g, u8g_font_helvB10);
-    Display_Write_CenteredP(strP);
-  } while(u8g_NextPage(&u8g));
-}
-
-void Display_USB_Unknown(void)
-{
-  u8g_FirstPage(&u8g);
-  do {
-    u8g_SetFont(&u8g, u8g_font_helvB10);
-    Display_Write_CenteredP(U8G_PSTR("USB\nunknown\nstate"));
-  } while(u8g_NextPage(&u8g));
-}
-
-void Display_Status(void)
-{
-  switch(USB_DeviceState){
+  switch(context->USB_DeviceState){
+    /**< Internally implemented by the library. This state indicates
+      *   that the device is not currently connected to a host.
+      */
     case DEVICE_STATE_Unattached:
-      Display_USB_Unattached();
+      Display_Write_CenteredP(U8G_PSTR("USB\ndisconnected"));
       break;
+    /**< Internally implemented by the library. This state indicates
+      *   that the device is connected to a host, but enumeration has not
+      *   yet begun.
+      */
     case DEVICE_STATE_Powered:
-      Display_USB_Powered();
+      Display_Write_CenteredP(U8G_PSTR("USB connected,\nwaiting for\nenumeration"));
       break;
+    /**< Internally implemented by the library. This state indicates
+      *   that the device's USB bus has been reset by the host and it is
+      *   now waiting for the host to begin the enumeration process.
+      */
     case DEVICE_STATE_Default:
-      Display_USB_Default();
+      Display_Write_CenteredP(U8G_PSTR("USB bus reset,\nwaiting for\nenumeration"));
       break;
+    /**< Internally implemented by the library. This state indicates
+      *   that the device has been addressed by the USB Host, but is not
+      *   yet configured.
+      */
     case DEVICE_STATE_Addressed:
-      Display_USB_Addressed();
+      Display_Write_CenteredP(U8G_PSTR("USB addressed,\nwaiting for\nconfiguration"));
       break;
+    /**< May be implemented by the user project. This state indicates
+      *   that the device has been enumerated by the host and is ready
+      *   for USB communications to begin.
+      */
     case DEVICE_STATE_Configured:
-      Display_USB_Configured();
+      if(context->Right_Side_Disconnected) {
+        Display_Write_CenteredP(U8G_PSTR("Right Side\nDisconnected"));
+      } else {
+        char str[20];
+
+        u8g_SetFont(&u8g, u8g_font_helvB14);
+        Display_Draw_LED_Status(context->LEDReport);
+        Display_Draw_Layout_Status(context->KeyboardLayerName, context->ComputerLayerName);
+
+        u8g_SetFont(&u8g, u8g_font_6x10);
+        ultoa(context->Counter, str, 10);
+        u8g_DrawStr(&u8g, u8g_GetWidth(&u8g) / 2 - u8g_GetStrWidth(&u8g, str) / 2, u8g_GetHeight(&u8g), str);
+      }
       break;
+    /**< May be implemented by the user project. This state indicates
+      *   that the USB bus has been suspended by the host, and the device
+      *   should power down to a minimal power level until the bus is
+      *   resumed.
+      */
     case DEVICE_STATE_Suspended:
-      Display_USB_Suspended();
+      if(USB_Device_RemoteWakeupEnabled)
+        Display_Write_CenteredP(U8G_PSTR("USB suspended:\npress any key to\nwake up"));
+      else
+        Display_Write_CenteredP(U8G_PSTR("USB\nsuspended"));
       break;
     default:
-      Display_USB_Unknown();
+      Display_Write_CenteredP(U8G_PSTR("USB\nunknown\nstate"));
       break;
   }
 }
 
-void Display_Screensaver(void)
+struct ScreensaverContext
 {
-  // New pattern
-  if(Timer_Secs() - last_display_init > 3)
-    Display_Screensaver_Init();
+  uint8_t px;
+  uint8_t py;
+  uint8_t step;
+  uint8_t start;
+  uint32_t last_display_init;
+};
 
-  u8g_FirstPage(&u8g);
-  do {
-    if(step%2) {
-      for(uint8_t x=start; x < u8g_GetWidth(&u8g) ; x += step)
-        u8g_DrawLine(&u8g, x, 0, px, py);
-      for(uint8_t x=start; x < u8g_GetWidth(&u8g) ; x += step)
-        u8g_DrawLine(&u8g, x, u8g_GetHeight(&u8g), px, py);
-    }else{
-      for(uint8_t y=start; y < u8g_GetHeight(&u8g) ; y += step)
-        u8g_DrawLine(&u8g, 0, y, px, py);
-      for(uint8_t y=start; y < u8g_GetHeight(&u8g) ; y += step)
-        u8g_DrawLine(&u8g, u8g_GetWidth(&u8g), y, px, py);
-    }
-  } while(u8g_NextPage(&u8g));
+void *Display_Screensaver_Context(void)
+{
+  static struct ScreensaverContext context;
+  static uint32_t last_context = 0;
+
+  if(!last_context || Timer_Secs() - last_context > 3) {
+    context.px = (float)rand() / RAND_MAX * u8g_GetWidth(&u8g);
+    context.py = (float)rand() / RAND_MAX * u8g_GetHeight(&u8g);
+    context.step = (float)rand() / RAND_MAX * 22 + 12;
+    context.start = (float)rand() / RAND_MAX * context.step;
+    context.last_display_init = Timer_Secs();
+    last_context = Timer_Secs();
+  }
+
+  return (void *)&context;
+}
+
+void Display_Screensaver(void *in_context)
+{
+  struct ScreensaverContext *context;
+
+  context = (struct ScreensaverContext *)in_context;
+
+  if(context->step%2) {
+    for(uint8_t x=context->start; x < u8g_GetWidth(&u8g) ; x += context->step)
+      u8g_DrawLine(&u8g, x, 0, context->px, context->py);
+    for(uint8_t x=context->start; x < u8g_GetWidth(&u8g) ; x += context->step)
+      u8g_DrawLine(&u8g, x, u8g_GetHeight(&u8g), context->px, context->py);
+  }else{
+    for(uint8_t y=context->start; y < u8g_GetHeight(&u8g) ; y += context->step)
+      u8g_DrawLine(&u8g, 0, y, context->px, context->py);
+    for(uint8_t y=context->start; y < u8g_GetHeight(&u8g) ; y += context->step)
+      u8g_DrawLine(&u8g, u8g_GetWidth(&u8g), y, context->px, context->py);
+  }
 }
 
 /*
  * Update
  */
 
-void Display_Update(void)
+void Display_PrepareUpdate(void (**draw)(void *), void **context)
 {
-  if(USB_DeviceState != last_USB_DeviceState) {
-    Display_Tick();
-    last_USB_DeviceState = USB_DeviceState;
-  }
-
   switch(mode){
     case SPLASH_MODE:
       if(Timer_Secs() > SPLASH_TIMEOUT){
         Display_Tick();
         mode = STATUS_MODE;
-      } else
-        Display_Splash();
-      break;
+      } else{
+        *draw = &Display_Splash;
+        *context = NULL;
+        break;
+      }
     case STATUS_MODE:
       if(Timer_Secs() - last_tick > SCREENSAVER_TIMEOUT) {
-        Display_Screensaver_Init();
         mode = SCREENSAVER_MODE;
-      } else
-        Display_Status();
-      break;
+      } else {
+        *draw = &Display_Status;
+        *context = Display_Status_Context();
+        break;
+      }
     case SCREENSAVER_MODE:
       if(Timer_Secs() - last_tick > SCREENSAVER_TIMEOUT) {
         u8g_SetContrast(&u8g, 0);
-        Display_Screensaver();
+        *draw = &Display_Screensaver;
+        *context = Display_Screensaver_Context();
+        break;
       } else {
         mode = STATUS_MODE;
         u8g_SetContrast(&u8g, CONTRAST);
       }
-      break;
+  }
+}
+
+void Display_Update(void)
+{
+  static uint8_t updating = 0;
+  static void (*draw)(void *) = NULL;
+  static void *context = NULL;
+
+  if(USB_DeviceState != last_USB_DeviceState) {
+    Display_Tick();
+    last_USB_DeviceState = USB_DeviceState;
+  }
+
+  if(!updating) {
+    Display_PrepareUpdate(&draw, &context);
+    u8g_FirstPage(&u8g);
+    draw(context);
+    updating = 1;
+  }else{
+    if(u8g_NextPage(&u8g)){
+      draw(context);
+    }else{
+      updating = 0;
+    }
   }
 }
 
